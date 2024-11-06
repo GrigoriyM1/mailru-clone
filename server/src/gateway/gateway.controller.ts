384d568fc085895b2037408facc1485de4e8d0a2
GrigoriyM1 <alek9051@gmail.com>
2024-11-06 10:45:47 +0400

    adsasd

--- server/src/gateway/gateway.controller.ts
+++ server/src/gateway/gateway.controller.ts
@@ -13,59 +13,59 @@ import { GatewayService } from "./gateway.service";
   },
   namespace: '/'
 })
-export class GatewayController implements OnModuleInit {
+export class GatewayController {
   constructor(private readonly gatewayService: GatewayService) { }
 
   @WebSocketServer()
   server: Server;
 
-  private questionSockets: Map<number, Set<string>> = new Map();
+  // private questionSockets: Map<number, Set<string>> = new Map();
 
-  onModuleInit() {
-    // this.server.on('connection', socket => {
-      // console.log('Connected ', socket.id);
-    // });
-    console.log('server  ', this.server)
-  }
+  // onModuleInit() {
+  //   // this.server.on('connection', socket => {
+  //     // console.log('Connected ', socket.id);
+  //   // });
+  //   console.log('server  ', this.server)
+  // }
 
-  @SubscribeMessage('joinQuestion')
-  handleJoinQuestion(
-    @MessageBody() body: { questionId: number }, 
-    @ConnectedSocket() socket: Socket
-  ) {
-    const { questionId } = body;
+  // @SubscribeMessage('joinQuestion')
+  // handleJoinQuestion(
+  //   @MessageBody() body: { questionId: number }, 
+  //   @ConnectedSocket() socket: Socket
+  // ) {
+  //   const { questionId } = body;
 
-    if (!this.questionSockets.has(questionId)) {
-      this.questionSockets.set(questionId, new Set());
-    }
-    this.questionSockets.get(questionId).add(socket.id); 
-  }
+  //   if (!this.questionSockets.has(questionId)) {
+  //     this.questionSockets.set(questionId, new Set());
+  //   }
+  //   this.questionSockets.get(questionId).add(socket.id); 
+  // }
 
   @SubscribeMessage('newQuestion')
   onNewQuestion(@MessageBody() body: any) {
     this.server.emit('newQuestion', { data: body });
   }
 
-  @SubscribeMessage('newAnswer')
-  onNewAnswer(@MessageBody() body: { questionId: number, answer: any }) {
-    const sockets = this.questionSockets.get(body.questionId);
+  // @SubscribeMessage('newAnswer')
+  // onNewAnswer(@MessageBody() body: { questionId: number, answer: any }) {
+  //   const sockets = this.questionSockets.get(body.questionId);
 
-    // console.log('newanswer ', body)
+  //   // console.log('newanswer ', body)
     
-    if (sockets) {
-      sockets.forEach(socketId => {
-        this.server.to(socketId).emit('newAnswer', { data: body }); // Отправляем только нужным сокетам
-      });
-    }
-  }
+  //   if (sockets) {
+  //     sockets.forEach(socketId => {
+  //       this.server.to(socketId).emit('newAnswer', { data: body }); // Отправляем только нужным сокетам
+  //     });
+  //   }
+  // }
 
-  @SubscribeMessage('disconnect')
-  handleDisconnect(socket: any) {
-    this.questionSockets.forEach((sockets, questionId) => {
-      sockets.delete(socket.id);
-      if (sockets.size === 0) {
-        this.questionSockets.delete(questionId); // Удаляем, если нет активных сокетов
-      }
-    });
-  }
+  // @SubscribeMessage('disconnect')
+  // handleDisconnect(socket: any) {
+  //   this.questionSockets.forEach((sockets, questionId) => {
+  //     sockets.delete(socket.id);
+  //     if (sockets.size === 0) {
+  //       this.questionSockets.delete(questionId); // Удаляем, если нет активных сокетов
+  //     }
+  //   });
+  // }
 }
